c24c947bc0e60ca1bc3a7270864361249537927d
dzakyy04 <dzakylinggau@gmail.com>
2023-05-02 10:56:40 +0700

    Create Page Component

--- survey-frontend/src/router.jsx
+++ survey-frontend/src/router.jsx
@@ -1,18 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import Surveys from "./views/Surveys";
 import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Dashboard />,
+    path: "/dashboard",
+    element: <Navigate to="/" />
   },
   {
-    path: "/surveys",
-    element: <Surveys />,
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />,
+      },
+    ],
   },
   {
     path: "/",
